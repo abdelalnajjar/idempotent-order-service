33580692f8532f4877be361ca28f4146cdca7a37
AbdelRahman Alnajjar <>
2026-02-24 13:08:13 -0800

    Add /reset route, implement structured logging, and wrap order creation in DB transaction (BEGIN/COMMIT/ROLLBACK) for atomic consistency

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const PORT = 3000
 
 app.use(express.json());
 
+// A middleware to add a uniqe id to the incoming request
+app.use((req, res, next) => {
+  req.requestId = uuidv4();
+  next();
+});
+
 function hash(str) {
   return crypto.createHash("sha256").update(str).digest("hex");
 }
@@ -50,7 +56,7 @@ db.run(`
 `);
 
 db.run(`
-  CREATE TABLE IF NOT EXISTS idempotency_keys  (
+  CREATE TABLE IF NOT EXISTS idempotency_records  (
     key TEXT PRIMARY KEY,
     request_hash TEXT,
     response_body TEXT,
@@ -68,72 +74,131 @@ app.get("/",(req,res)=>{
 
 app.post("/orders", async (req, res) => {
   try {
+    console.log("\n-----------------------------------------------------------------------")
     const { customer_id, item_id, quantity } = req.body;
+
     // make sure we have a valid body
     if (!customer_id || !item_id || !Number.isInteger(quantity) || quantity <= 0)
         return res.status(400).json({ error: "Invalid request body" });
     const orderData = { customer_id, item_id, quantity };// Stringify the request, hash it, then compare it with the idempotency key's hashed request  
     const idempotencyKey = req.get("Idempotency-Key");
+    
+    // received an order
+    console.log(JSON.stringify({
+      event: "order_request_received",
+      request_id: req.requestId,
+      idempotencyKey,
+      body: orderData
+    }));
+    
+
     if (!idempotencyKey) return res.status(400).json({ error: "Missing Idempotency-Key" });
     const failureCase = req.get('X-Debug-Fail-After-Commit') === 'true';
 
-    console.log("failureCase is: "+failureCase)
+    console.log("### failureCase is: "+failureCase)
 
     const order_id = uuidv4()
     const ledger_id = uuidv4()
 
-    const idempotency_key = await getQuery(
-      "SELECT * FROM idempotency_keys WHERE key = ?",
+    const idempotency_record = await getQuery(
+      "SELECT * FROM idempotency_records WHERE key = ?",
       [idempotencyKey]
     );
 
     // if the idempotency key already exist, check if same payload or not 
-    if (idempotency_key) {
+    if (idempotency_record) {
         const newOrderHash = hash(JSON.stringify(orderData));
-        if(idempotency_key.request_hash !== newOrderHash){
-            console.log("Different order BUT same KEY")
-            return res.status(409).json({ error: "Idempotency key reuse with different payload" });
+        if(idempotency_record.request_hash !== newOrderHash){
+            
+          console.log(JSON.stringify({
+            event: "idempotency_conflict",
+            request_id: req.requestId,
+            idempotencyKey,
+            status_code: 409
+          }));
+          
+          return res.status(409).json({ error: "Idempotency key reuse with different payload" });
         }
-        console.log("Already ordered! NO ACTION NEEDED")
-        // console.log(idempotency_key.request_hash)
-        // console.log("Returning status:", idempotency_key.status_code);
-        // console.log("Returning body:", JSON.parse(idempotency_key.response_body));
-        return res.status(idempotency_key.status_code).json(JSON.parse(idempotency_key.response_body));
+        // console.log("Already ordered! NO ACTION NEEDED")
+        console.log(JSON.stringify({
+          event: "idempotent_retry",
+          request_id: req.requestId,
+          idempotencyKey,
+          status_code: idempotency_record.status_code
+        }));
+        return res.status(idempotency_record.status_code).json(JSON.parse(idempotency_record.response_body));
     }
 
-    // order not yet created; insert into orders table
-    console.log("Creating order!")
-    await runQuery(
-        "INSERT INTO orders (order_id, customer_id, item_id, quantity) VALUES (?, ?, ?, ?)",
-        [order_id, customer_id, item_id, quantity]
-    );
-    console.log("Adding ledger entry!")
-    await runQuery(
-        "INSERT INTO ledger (ledger_id, order_id, amount) VALUES (?, ?, ?)",
-        [ledger_id,order_id, quantity*2]
-    );
 
 
-    const responseBody = JSON.stringify({ order_id, status: "created" });
-    const requestBodyHash =  hash(JSON.stringify(orderData));
-    // insert new idempotency record
-    await runQuery(
-        "INSERT INTO idempotency_keys (key, request_hash, response_body, status_code, created_at) VALUES (?, ?, ?, ?, ?)",
-        [idempotencyKey, requestBodyHash, responseBody, 201, new Date().toISOString()]
-    );
+    
+    // One Transaction  = insert order entry + insert ledge entry + insert idempotency_record entry 
+    await runQuery("BEGIN TRANSACTION");
+
+    try{
+      
+      const responseBody = JSON.stringify({ order_id, status: "created" });
+      const requestBodyHash =  hash(JSON.stringify(orderData));
+      // insert new idempotency record
+      await runQuery(
+          "INSERT INTO idempotency_records (key, request_hash, response_body, status_code, created_at) VALUES (?, ?, ?, ?, ?)",
+          [idempotencyKey, requestBodyHash, responseBody, 201, new Date().toISOString()]
+      );
+
+      // order not yet created; insert into orders table
+      console.log("Creating order!")
+      await runQuery(
+          "INSERT INTO orders (order_id, customer_id, item_id, quantity) VALUES (?, ?, ?, ?)",
+          [order_id, customer_id, item_id, quantity]
+      );
+      // console.log("Adding ledger entry!")
+      await runQuery(
+          "INSERT INTO ledger (ledger_id, order_id, amount) VALUES (?, ?, ?)",
+          [ledger_id,order_id, quantity*2]
+      );
+
+
+
+      // commit the transaction
+      console.log(JSON.stringify({
+        event: "order_committed",
+        request_id: req.requestId,
+        order_id,
+        idempotencyKey,
+        status_code: 201
+      }));
+      await runQuery("COMMIT");
+
+
+    }catch(error){
+      await runQuery("ROLLBACK");
+      throw error; 
+    }
 
 
     if (failureCase) {
-        req.socket.destroy(); // simulates dropped connection after commit
-        return;
+      console.log(JSON.stringify({
+        event: "fail_after_commit",
+        request_id: req.requestId,
+        order_id,
+        idempotencyKey,
+        message: "Dropped connection after DB commit"
+      }));
+      req.socket.destroy(); // simulates dropped connection after commit
+      return;
     }
 
     // if(failureCase)
     //     return res.status(500).json({ error: "Internal Server Error, try again!" });
     return res.status(201).json({ order_id, status: "created" });
   } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: "Database error" });
+    console.log(JSON.stringify({
+      event: "server_error",
+      request_id: req.requestId,
+      error: err.message,
+      status_code: 500
+    }));
+    return res.status(500).json({ error: "Server error" });
   }
 });
 
@@ -155,6 +220,37 @@ app.get("/orders/:order_id",async (req,res)=>{
     }catch(error){
         console.log(error);
         return res.status(500).json({ error: "Database error" });
+    }
+
+})
+
+
+// a route to reset the database, 
+app.post("/reset",async (req,res)=>{
+    
+    try{
+      await runQuery("BEGIN TRANSACTION");
+      await runQuery("DELETE FROM orders");
+      await runQuery("DELETE FROM ledger");
+      await runQuery("DELETE FROM idempotency_records");
+      await runQuery("COMMIT");
+      
+      console.log(JSON.stringify({
+        event: "database_reset_success",
+        request_id: req.requestId,
+        status_code: 200
+      }));
+      
+      return res.status(200).json({ message: "Database reset successful" });
+    }catch(error){
+      await runQuery("ROLLBACK");
+      
+      console.log(JSON.stringify({
+        event: "database_reset_failed",
+        request_id: req.requestId,
+        status_code: 500
+      }));
+      return res.status(500).json({ error: "(RESET) Database error" });
     }
 
 })
